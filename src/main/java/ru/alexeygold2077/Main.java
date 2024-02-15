fa48d5a94041ced8dd197fa841fb85df15886646
AlexeyGold2077 <alexgold2077@gmail.com>
2024-02-15 20:04:57 +0300

    v1.0

--- src/main/java/ru/alexeygold2077/Main.java
+++ src/main/java/ru/alexeygold2077/Main.java
@@ -1,22 +1,8 @@
 package ru.alexeygold2077;
 
-import java.io.IOException;
-
 public class Main {
 	
 	public static void main(String[] args) {
-		TestClass testClass = new TestClass();
-		Serializator<TestClass> serializator = new Serializator<>(testClass, "obj.bin");
-
-		try {
-			serializator.Serialize();
-		} catch (IOException ignored) {}
-
-		try {
-			TestClass testClass2 = serializator.Deserialize();
-			System.out.println(testClass2.toString());
-		} catch (IOException | ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
+		
     }
 }
